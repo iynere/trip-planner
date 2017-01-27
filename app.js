bab6377b7494d93cb050a86d7cbba96f63007bc8
Christine <cleverett1@gmail.com>
2017-01-27 15:25:56 -0500

    style outline

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var swig = require('swig');
+var morgan = require('morgan');
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.set('view cache', false);
 // all environments
 app.set('port', process.env.PORT || 3000); //allows us to say app.get('port') <-- very different from app.get that you are used to, same name but different implementation because of the number and type of parameters. This is a poor convention
 app.use(express.favicon());
-app.use(express.logger('dev')); //just like morgan but with express (only for express3)
+app.use(morgan('dev')); 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
@@ -36,8 +37,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/contact', routes.contact);
+app.get('/', routes.contact);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
